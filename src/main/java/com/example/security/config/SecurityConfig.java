76e6cc4d118bd47cf9b652f6652cd1c239d884e5
deviloper <developer.jhlee@gmail.com>
2022-02-07 09:48:18 +0900

    feat: immemory security config - one user

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,31 +1,73 @@
 package com.example.security.config;
 
+import com.example.security.domain.user.LoginUserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  //  final LoginUserDetailsServiceImpl loginUserDetailsService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests(
             (authorize) ->
                 authorize
-                    .antMatchers("/static/**", "/index", "/user/**")
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                     .permitAll()
-                    .antMatchers("/info/**")
-                    .hasRole("USER"))
-        .formLogin(
-            (formLogin) -> formLogin.loginPage("/user/login").failureUrl("/user/login-error"));
+                    .antMatchers("/", "/index", "/user/**", "/h2-console/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .formLogin();
+    //        .formLogin(
+    //            (formLogin) ->
+    // formLogin.loginPage("/user/login").failureUrl("/user/login-error"));
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder(){
-    return new BCryptPasswordEncoder();
+  public UserDetailsService userDetailsService() {
+    UserDetails userDetails =
+        User.withUsername("test")
+            .password(passwordEncoder().encode("test1234"))
+            .roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(userDetails);
   }
 
+  //  @Bean
+  //  public DaoAuthenticationProvider authenticationProvider() {
+  //    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+  //    authenticationProvider.setUserDetailsService(userDetailsService());
+  //    authenticationProvider.setPasswordEncoder(passwordEncoder());
+  //    return authenticationProvider;
+  //  }
+  //
+  //  @Bean
+  //  public UserDetailsService userDetailsService() {
+  //    return loginUserDetailsService;
+  //  }
+  //
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- src/main/java/com/example/security/domain/user/LoginUserDetailsServiceImpl.java
+++ src/main/java/com/example/security/domain/user/LoginUserDetailsServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
-@Service
+//@Service
 public class LoginUserDetailsServiceImpl implements UserDetailsService {
 
   final UserRepository userRepository;
