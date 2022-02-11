a9fc1e2db0743c6302ae6c1f77430c886dc6ca6b
deviloper <developer.jhlee@gmail.com>
2022-02-11 13:53:39 +0900

    feat: add regist api

--- src/main/java/com/example/security/domain/user/UserApiController.java
+++ src/main/java/com/example/security/domain/user/UserApiController.java
@@ -1,9 +1,13 @@
 package com.example.security.domain.user;
 
 import com.example.security.domain.user.entity.ServiceUser;
+import com.example.security.domain.user.form.RegistForm;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +19,12 @@ public class UserApiController {
   final UserService userService;
 
   @GetMapping("/{no}")
-  public ServiceUser serviceUserByNo(@PathVariable("no") Long no) {
-    return userService.getServiceUser(no);
+  public ResponseEntity<ServiceUser> serviceUserByNo(@PathVariable("no") Long no) {
+    return ResponseEntity.ok(userService.getServiceUser(no));
   }
 
+  @PostMapping("/regist")
+  public ResponseEntity<ServiceUser> regist(@Valid RegistForm registForm) {
+    return ResponseEntity.ok(userService.regist(registForm));
+  }
 }
--- src/main/java/com/example/security/domain/user/form/RegistForm.java
+++ src/main/java/com/example/security/domain/user/form/RegistForm.java
@@ -1,5 +1,8 @@
 package com.example.security.domain.user.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +18,17 @@ import lombok.ToString;
 @ToString
 public class RegistForm {
 
+  @NotBlank(message = "아이디를 입력해주세요.")
+  @Size(min = 3, max = 255, message = "유저아이디는 3자리이상 255이하 가능합니다.")
   private String userId;
+
+  @NotBlank(message = "이름을 입력해주세요.")
   private String userName;
+
+  @NotBlank(message = "패스워드는 12자이상 입력해주세요.")
+  @Size(min = 12, max = 128, message = "비밀번호는 12자리이상 128자리이하 가능합니다.")
   private String password;
+
+  @Email(message = "이메일을 입력해주세요.")
   private String email;
 }
